4692f0719c060d66f6e87f2f2da0df48ddbfee91
AnastasiiaG <lottaanastasia@gmail.com>
2023-10-06 16:06:51 +0300

    feaet: через попапы можно добавить данные о профиле и новую карточку

--- index.js
+++ index.js
@@ -1,34 +1,53 @@
 const editButton = document.querySelector('.profile__edit');
 const addButton = document.querySelector('.profile_add');
 const popup = document.querySelector('.popup');
-const buttonClosePopup = document.querySelector('.popup__close');
+const popupProfile = document.querySelector('.popup__profile')
 const profileName = document.querySelector('.profile__name');
 const profileAbout = document.querySelector('.profile__cuption');
-const popupName = document.querySelector('.popup__name');
-const popupAbout = document.querySelector('.popup__about');
+const popupPlace = document.querySelector('.popup__place');
+const placeTitleInput = popupPlace.querySelector('.place__title');
+const placeLinkInput = popupPlace.querySelector('.place__link');
+const buttonClosePopup = popup.querySelector('.popup__close');
+const popupName = popup.querySelector('.popup__name');
+const popupAbout = popup.querySelector('.popup__about');
 const likeButton = document.querySelector('.gallery__button');
-const templateItem = document
-.querySelector('.template-item').content.querySelector('.gallery__item');
+const templateItem = document.querySelector('.template-item').content.querySelector('.gallery__item');
 const gallerySection = document.querySelector('.gallery');
-
+const profileForm = document.querySelector('.profile__form');
+const placeForm = document.querySelector('.place__form');
 
 popupName.value = profileName.textContent;
 popupAbout.value = profileAbout.textContent;
 
-// function showPopup (popup) {
-//   popup.classList.add('popup_opened');
-// }
-
-// function closePopup(popup) {
-//   popup.classList.remove('popup_opened');
-// }
-
-// addButton.addEventListener('click', showPopup(popup));
-// addButton.addEventListener('click', closePopup(popup));
-
-function LikePhoto () {
-likeButton.classList.add('gallery__button_liked');
+function showPopup (popup) {
+  popup.classList.add('popup_opened');
 }
+
+function closePopup(popup) {
+  popup.classList.remove('popup_opened');
+}
+
+editButton.addEventListener('click', () => showPopup(popupProfile));
+buttonClosePopup.addEventListener('click', () => closePopup(popupProfile));
+addButton.addEventListener('click', () => showPopup(popupPlace));
+buttonClosePopup.addEventListener('click', () => closePopup(popupPlace));
+
+function createGalleryItem(galleryName, galleryPhoto) {
+  const newGalleryItem = templateItem.cloneNode(true);
+  const galleryHeader = newGalleryItem.querySelector('.gallery__header');
+  galleryHeader.textContent = galleryName;
+  const galleryImage = newGalleryItem.querySelector('.gallery__image');
+  galleryImage.src = galleryPhoto;
+  return newGalleryItem;
+}
+
+function addCard(element) {
+  gallerySection.prepend(element);
+}
+
+// function LikePhoto () {
+// likeButton.classList.add('gallery__button_liked');
+// }
 // likeButton.addEventListener('click', LikePhoto);
 
 const initialCards = [
@@ -58,19 +77,22 @@ const initialCards = [
   }
 ];
 
-function createGalleryItem(galleryName, galleryPhoto) {
-  const newGalleryItem = templateItem.cloneNode(true);
-  const galleryHeader = newGalleryItem.querySelector('.gallery__header');
-  galleryHeader.textContent = galleryName;
-  const galleryImage = newGalleryItem.querySelector('.gallery__image');
-  galleryImage.src = galleryPhoto;
-  return newGalleryItem;
-}
-
-function addCard(element) {
-  gallerySection.prepend(element);
-}
-
 initialCards.forEach(obj => {
   addCard(createGalleryItem(obj['name'], obj['link']))
 })
+
+function handleFormSubmitProfile(evt) {
+  evt.preventDefault(); 
+  profileName.textContent = popupName.value;
+  profileAbout.textContent = popupAbout.value;
+  closePopup(popupProfile)
+}
+
+function handleFormSubmitNewPlace(evt) {
+  evt.preventDefault(); 
+  addCard(createGalleryItem(placeTitleInput.value, placeLinkInput.value))
+  closePopup(popupPlace)
+}
+
+profileForm.addEventListener('submit', handleFormSubmitProfile);
+placeForm.addEventListener('submit', handleFormSubmitNewPlace)
