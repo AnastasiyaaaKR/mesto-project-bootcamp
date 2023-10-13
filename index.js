79142844410ddf94ed2b8d3f10668aede210a062
AnastasiiaG <lottaanastasia@gmail.com>
2023-10-13 10:35:00 +0300

    feat: добавлена валидация формы на ввод данных в попапе профиля

--- index.js
+++ index.js
@@ -2,19 +2,23 @@ const editButton = document.querySelector('.profile__edit');
 const addButton = document.querySelector('.profile_add');
 const popup = document.querySelector('.popup');
 const popupProfile = document.querySelector('.popup__profile');
-const popupName = popupProfile.querySelector('.popup__name');
-const popupAbout = popupProfile.querySelector('.popup__about');
-const profileName = document.querySelector('.profile__name');
+const popupName = popupProfile.querySelector('.popup__name'); //Имя пользователя
+const popupAbout = popupProfile.querySelector('.popup__about'); //О себе
+const profileName = document.querySelector('.profile__name'); 
 const profileAbout = document.querySelector('.profile__cuption');
 const popupPlace = document.querySelector('.popup__place');
 const placeTitleInput = popupPlace.querySelector('.place__title');
 const placeLinkInput = popupPlace.querySelector('.place__link');
-const buttonsClosePopup = document.querySelectorAll('.popup__close');
+const ClosePopupbuttons = document.querySelectorAll('.popup__close');
 const templateItem = document.querySelector('.template-item').content.querySelector('.gallery__item');
 const gallerySection = document.querySelector('.gallery');
 const popupPhoto = document.querySelector('.popup__photo');
-const profileForm = document.forms.profileForm;
-const placeForm = document.forms.placeForm;
+const popupPhotoContent = document.querySelector('.popup__photo-content');
+const popupPhotoName = document.querySelector('.popup__photo-name');
+const buttonSubmit = document.querySelector('.popup__button') //кнопка сабмита
+const profileForm = document.forms.profileForm; //форма профиля
+const placeForm = document.forms.placeForm; //форма карточки
+const inputFields = profileForm.querySelectorAll('.popup__text'); //вытаскиваю все инпуты из попапа профиля
 
 function showPopup (popup) {
   popup.classList.add('popup_opened');
@@ -26,7 +30,7 @@ function closePopup(evt) {
   popup.classList.remove('popup_opened');
 }
 
-for(const button of buttonsClosePopup) {
+for(const button of ClosePopupbuttons) {
   button.addEventListener('click', (evt) =>  closePopup((evt)));
 }
 
@@ -52,14 +56,13 @@ function createGalleryItem(galleryName, galleryPhoto) {
   galleryHeader.textContent = galleryName;
   const galleryImage = newGalleryItem.querySelector('.gallery__image');
   galleryImage.src = galleryPhoto;
+  galleryImage.alt = galleryName;
   const galleryDeliteButton = newGalleryItem.querySelector('.gallery__trash');
   galleryDeliteButton.addEventListener('click', () => deliteCard(newGalleryItem))
   const likeButton = newGalleryItem.querySelector('.gallery__button');
   likeButton.addEventListener('click', likeCard);
   galleryImage.addEventListener('click', () => {
-  const popupPhotoContent = document.querySelector('.popup__photo-content');
   popupPhotoContent.src = galleryImage.src;
-  const popupPhotoName = document.querySelector('.popup__photo-name');
   popupPhotoName.textContent = galleryHeader.textContent;
   showPopup(popupPhoto)
   });
@@ -112,9 +115,61 @@ function handleFormSubmitNewPlace(evt) {
   evt.preventDefault(); 
   addCard(createGalleryItem(placeTitleInput.value, placeLinkInput.value));
   closePopup(evt);
-  placeTitleInput.value = '';
-  placeLinkInput.value = '';
+  evt.target.reset()
 }
 
 profileForm.addEventListener('submit', handleFormSubmitProfile);
-placeForm.addEventListener('submit', handleFormSubmitNewPlace)
+placeForm.addEventListener('submit', handleFormSubmitNewPlace);
+
+//код для форм
+
+function showErorMessage(input, errorMessage) {
+  const spanField = 'eror-' + input.id;
+  const erorElement = document.getElementById(spanField);
+  erorElement.textContent = errorMessage;
+}
+
+function hideErrorMessage(input) {
+  const spanField = 'eror-' + input.id;
+  const erorElement = document.getElementById(spanField);
+  erorElement.textContent = '';
+}
+
+function checkFieldValidity(input) {
+    if (input.validity.valid) {
+    hideErrorMessage(input);
+  } else {
+    showErorMessage(input, input.validationMessage)
+  }
+}
+
+// function handleFormSubmitForm() {
+//   const inputField = popupName;
+// }
+// profileForm.addEventListener('submit', handleFormSubmitForm);
+
+function turnOfftheSubmitButton (button) { //делаем кнопку неактивной
+  button.disabled = true;
+}
+
+function turnOntheSubmitButton(button) {
+  button.disabled = false;
+}
+
+function checkFormValidity(form, button) {
+  if(form.checkValidity()) {
+  turnOntheSubmitButton(button)
+  } else {
+  turnOfftheSubmitButton(button)
+  }
+}
+
+checkFormValidity(profileForm, buttonSubmit);
+
+inputFields.forEach(input => {
+  input.addEventListener('input', () => {
+    checkFormValidity(profileForm, buttonSubmit);
+    checkFieldValidity(input)
+  })
+});
+
