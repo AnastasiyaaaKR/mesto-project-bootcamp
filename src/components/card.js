f0e195376710ca51699a4cce75ef64c365847823
AnastasiiaG <lottaanastasia@gmail.com>
2023-10-24 19:34:04 +0200

    feat: сайт подключен к серверу, добавлен функционал отрисовки карточек с сайта, изменение и сохранение данных пользователя и отображение лайков каждой карточки

--- src/components/card.js
+++ src/components/card.js
@@ -1,4 +1,4 @@
-import { templateItem, gallerySection, popupPhoto, popupPhotoContent, popupPhotoName} from './../index.js'
+import { templateItem, gallerySection, popupPhoto, popupPhotoContent, popupPhotoName, counterLikes} from './../index.js'
 import { showPopup } from './modal.js';
 
 function deliteCard(item) {
@@ -9,15 +9,18 @@ function likeCard(evt) {
   evt.target.closest('.gallery__button').classList.toggle('gallery__button_liked');
 }
 
-function createGalleryItem(galleryName, galleryPhoto) {
+
+function createGalleryItem(galleryName, galleryPhoto, counterLikes) {
   const newGalleryItem = templateItem.cloneNode(true);
   const galleryHeader = newGalleryItem.querySelector('.gallery__header');
   galleryHeader.textContent = galleryName;
   const galleryImage = newGalleryItem.querySelector('.gallery__image');
   galleryImage.src = galleryPhoto;
   galleryImage.alt = galleryName;
+  const likes = newGalleryItem.querySelector('.gallery__likes');
+  likes.textContent = counterLikes;
   const galleryDeliteButton = newGalleryItem.querySelector('.gallery__trash');
-  galleryDeliteButton.addEventListener('click', () => deliteCard(newGalleryItem))
+  galleryDeliteButton.addEventListener('click', () => deliteCard(newGalleryItem));
   const likeButton = newGalleryItem.querySelector('.gallery__button');
   likeButton.addEventListener('click', likeCard);
   galleryImage.addEventListener('click', () => {
@@ -33,31 +36,5 @@ function addCard(element) {
   gallerySection.prepend(element);
 }
 
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
 
-export { createGalleryItem, addCard, initialCards};
+export { createGalleryItem, addCard };
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import './pages/index.css';
 import { checkFormValidity, enableValidation, turnOfftheSubmitButton } from './components/validate.js'; 
-import { createGalleryItem, addCard, initialCards} from './components/card.js';
+import { createGalleryItem, addCard } from './components/card.js';
 import {showPopup, closePopup} from './components/modal.js';
+import { getProfileInfo, getInitialCards, changeProfile, addNewCard } from './components/api.js'
 
 const editButton = document.querySelector('.profile__edit');
 const addButton = document.querySelector('.profile_add');
@@ -10,6 +11,7 @@ const popupName = popupProfile.querySelector('.popup__name');
 const popupAbout = popupProfile.querySelector('.popup__about');
 const profileName = document.querySelector('.profile__name'); 
 const profileAbout = document.querySelector('.profile__cuption');
+const profileImg = document.querySelector('.profile__img')
 const popupPlace = document.querySelector('.popup__place');
 const placeTitleInput = popupPlace.querySelector('.place__title');
 const placeLinkInput = popupPlace.querySelector('.place__link');
@@ -47,22 +49,26 @@ editButton.addEventListener('click', () => {
 
 addButton.addEventListener('click', () => showPopup(popupPlace));
 
-initialCards.forEach(obj => {
-  addCard(createGalleryItem(obj['name'], obj['link']))
-})
-
 function handleFormSubmitProfile(evt) {
   evt.preventDefault();
   profileName.textContent = popupName.value;
   profileAbout.textContent = popupAbout.value;
+  changeProfile(popupName.value, popupAbout.value)
+    .catch((err) => {
+      console.log(err);
+    });
   if(profileForm.checkValidity()) {
-  closePopup(popupProfile);
+    closePopup(popupProfile);
   }
 }
 
 function handleFormSubmitNewPlace(evt) {
   evt.preventDefault(); 
   addCard(createGalleryItem(placeTitleInput.value, placeLinkInput.value));
+  addNewCard(placeTitleInput.value, placeLinkInput.value)
+  .catch((err) => {
+      console.log(err);
+    });
   closePopup(popupPlace);
   evt.target.reset();
   turnOfftheSubmitButton(placeSubmitButton);
@@ -85,5 +91,26 @@ popups.forEach(popup => {
 });
 
 enableValidation(popupForms, validationSettings);
+
+getProfileInfo()
+  .then((result) => {
+    profileName.textContent = result.name;
+    profileAbout.textContent = result.about;
+    profileImg.src = result.avatar;
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+getInitialCards()
+.then((result) => {
+  result.forEach(res => {
+    addCard(createGalleryItem(res['name'], res['link'], res['likes'].length));
+  })
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 
 export { templateItem, gallerySection, popupPhoto, popupPhotoContent, popupPhotoName, editButton, addButton, closePopupbuttons, validationSettings}
